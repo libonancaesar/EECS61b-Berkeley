76d93628945c334dbade8a0aea59960733ee08f8
libonancaesar <libonancaesar@yahoo.com>
2022-06-05 16:49:28 -0500

    lab8 Bonan

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -31,18 +31,17 @@ public class MyHashMap<K, V> implements Map61B<K, V>{
 
 
     public MyHashMap() {
-        initialSize = INIT_SIZE;
-        loadFactor = LOAD_FACTOR;
+        this(INIT_SIZE, LOAD_FACTOR);
     }
 
     public MyHashMap(int initialSize) {
-        this.initialSize = initialSize;
-        loadFactor = LOAD_FACTOR;
+       this(initialSize, LOAD_FACTOR);
     }
 
     public MyHashMap(int initialSize, double loadFactor) {
         this.initialSize = initialSize;
         this.loadFactor = loadFactor;
+        map = (Item<K, V>[]) new Item[initialSize];
     }
 
     @Override
@@ -58,16 +57,44 @@ public class MyHashMap<K, V> implements Map61B<K, V>{
         return size;
     }
 
+    private void putHelper(K key, V value, Item item) {
+        if (item.key == key) {
+            item.val = value;
+        } else if (item.next == null){
+            item.next = new Item<>(key, value);
+            size = size + 1;
+        } else {
+            putHelper(key, value, item.next);
+        }
+    }
+
+    private int hashKeyFromTextBook(K key) {
+        return (key.hashCode() & 0x7fffffff) % initialSize;
+    }
 
     @Override
     public void put(K key, V val){
-        int index =  (key.hashCode() & 0x7fffffff) % initialSize;
+        int index =  hashKeyFromTextBook(key);
         if (map[index] == null) {
             map[index] = new Item(key, val);
             size = size + 1;
         } else {
-            /* are we putting the same value ? */
+            putHelper(key, val, map[index]);
         }
+        if ((double) size/initialSize > loadFactor) {
+            resize();
+        }
+
+    }
+
+    private void resize(){
+        MyHashMap<K, V> temp = new MyHashMap<>(initialSize * 2);
+        Set<K> keys = keySet();
+        for (K key: keys) {
+            temp.put(key, get(key));/* this get is to get the value from the current object */
+        }
+        initialSize = temp.initialSize;
+        map = temp.map;
     }
 
     @Override
@@ -82,7 +109,33 @@ public class MyHashMap<K, V> implements Map61B<K, V>{
         }
         return keys;
     }
+    private V getHelper(K key, Item item) {
+        if (item == null) {
+            return null;
+        } else if (item.key.equals(key)) {
+            return (V) item.val;
+        } else {
+            return (V) getHelper(key, item.next);
+        }
 
+    }
+
+    @Override
+    public V get(K key) {
+        int index = hashKeyFromTextBook(key);
+        return getHelper(key, map[index]);
+    }
+
+    @Override
+    public Iterator iterator(){
+        return new MyHashMapIter();
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int index = hashKeyFromTextBook(key);
+        return getHelper(key, map[index]) != null;
+    }
 
     @Override
     public V remove(K key){
@@ -94,4 +147,42 @@ public class MyHashMap<K, V> implements Map61B<K, V>{
         throw  new UnsupportedOperationException("MyHashMap doesn't support remove method.");
     }
 
+    private class MyHashMapIter implements Iterator<K> {
+        private Item cur;
+        private int index;
+        /**
+         * Create a new MyHashMapIter by setting cur to the first node in the
+         * linked list that stores the key-value pairs.
+         */
+        MyHashMapIter() {
+            index = 0;
+            cur = getItem(map[index]);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (cur != null);
+        }
+
+        @Override
+        public K next() {
+            K ret = (K) cur.key;
+            cur = getItem(cur.next);
+            return ret;
+        }
+
+        private Item getItem(Item cur) {
+            if (cur == null) {
+                index += 1;
+                if (index > initialSize - 1) {
+                    return null;
+                } else {
+                    return getItem(map[index]);
+                }
+            } else {
+                return cur;
+            }
+        }
+
     }
+}
